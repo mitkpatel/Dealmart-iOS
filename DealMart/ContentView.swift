f1232f40415553db65586209e15e4844b73ccabb
Mit <mitkpatel09@gmail.com>
2022-03-23 18:00:10 -0400

    Initial Commit

---
+++ DealMart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DealMart
+//
+//  Created by user215540 on 3/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DealMart/DealMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  DealMartApp.swift
+//  DealMart
+//
+//  Created by user215540 on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct DealMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
